592e7c89ad452801434e27a15cbd6db5cbd45f61
alefdodex <85092397+alefdodex@users.noreply.github.com>
2025-12-06 23:07:14 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,16 @@
 from telegram.ext import ApplicationBuilder, CommandHandler
-from config import TELEGRAM_BOT_TOKEN
+import os
+
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 
 async def start(update, context):
-    await update.message.reply_text("Bot sudah aktif, bro!")
-
-async def echo(update, context):
-    await update.message.reply_text(update.message.text)
+    await update.message.reply_text("Bot sudah aktif!")
 
 def main():
-    app = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
+    app = ApplicationBuilder().token(TOKEN).build()
 
+    # REGISTER COMMANDS YANG VALID
     app.add_handler(CommandHandler("start", start))
-    app.add_handler(CommandHandler("", echo))
 
     app.run_polling()
 
