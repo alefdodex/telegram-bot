b1d180d4c23ff9e90353cebaf23616ff50470954
alefdodex <85092397+alefdodex@users.noreply.github.com>
2025-12-06 21:56:42 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,20 @@
-print("Hello, your bot is ready!")
+import os
+import telegram
+from telegram.ext import Updater, CommandHandler
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+def start(update, context):
+    update.message.reply_text("Bot is running! Halo bossku! 🚀")
+
+def main():
+    updater = Updater(TOKEN, use_context=True)
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("start", start))
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == "__main__":
+    main()
